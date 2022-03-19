c55b1d9743897a157ff8cb86189fe5d20d63a53f
finder <0xfinder@protonmail.com>
2022-03-19 21:50:06 +0800

    progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,20 @@
 module.exports = {
-  siteMetadata: {
-    title: ``,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    siteMetadata: {
+        title: ``,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "pages"
-  }]
+    plugins: [
+        "gatsby-plugin-postcss",
+        "gatsby-plugin-mdx",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
+            },
+            __key: "pages",
+        },
+    ],
 };
