c0fb6bab82584c108b1a4c89bd8b7317c149156c
finder <0xfinder@protonmail.com>
2022-03-25 17:08:31 +0800

    format

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -5,7 +5,11 @@ import { OutboundLink } from "gatsby-plugin-google-analytics"
 const ExternalLink = forwardRef(({ href, children, ...rest }, ref) => {
   return (
     <Box as="span" {...rest} ref={ref}>
-      <OutboundLink rel="external noopener noreferrer" target="_blank" href={href}>
+      <OutboundLink
+        rel="external noopener noreferrer"
+        target="_blank"
+        href={href}
+      >
         {children}
       </OutboundLink>
     </Box>
